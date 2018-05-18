ea613d74c3b6f536ccc0d6e030038a9c58749d01
nielsswinkels <niels.sw@gmail.com>
2018-05-18 13:52:53 +0200

    Reduced speed for the normal+omni wheels. This speed makes it drive slow but still rotate like crazy.

--- motorcontrol/motorcontrol.ino
+++ motorcontrol/motorcontrol.ino
@@ -8,7 +8,7 @@ MeEncoderNew motor3(0x0a, SLOT1); // back right
 MeEncoderNew motor4(0x0a, SLOT2); // back left
 
 #define MAXSPEED 150
-#define DEFAULTSPEED 70
+#define DEFAULTSPEED 50
 #define COMMAND_TIMEOUT 300
 
 int moveSpeed = DEFAULTSPEED,motor1speed=0,motor2speed=0,motor3speed=0,motor4speed=0;
